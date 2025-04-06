bb90d910812e47ab45302eeba60f2937cdf3b4e6
fafafoyi <fatihefeunal236@gmail.com>
2025-04-06 03:08:05 +0200

    Debugged

--- VideoDownloader.py
+++ VideoDownloader.py
@@ -1,33 +1,44 @@
-from pytube import YouTube
 from tkinter import *
 import tkinter as tk
+import yt_dlp
 
 
 def downloader():
-    
     global res
     res = 0
-    
+
     t_url = t.get("1.0", "end").strip()
-    video = YouTube(t_url)
 
-    if res1.get() ==18:
-        res=18
-    elif res2.get() ==22:
-        res=22
-    elif res3.get() == 37:
-        res=37
+    try:
+        ydl_opts = {
+            'format': 'mp4',
+            'outtmpl': r"C:\Users\Hawei\Desktop\%(title)s.%(ext)s",
+
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([t_url])
+        for widget in root.pack_slaves():
+            if isinstance(widget, Label) and widget.cget("text") == "Downloaded Successfully" :
+                widget.destroy()
+
+        Label(root, text="Downloaded Successfully").pack()
+    except Exception as e:
+        # Shorten the error message for age restriction
+        error_message = str(e)
+        if "Sign in to confirm your age" in error_message:
+            error_message = "Error: Age-restricted video. Use cookies to bypass."
         
-    
-    video_streams = video.streams.filter(file_extension = 'mp4').get_by_itag(res)
-    video_streams.download(filename = "Untitled.mp4", output_path = r"C:\Users\Hawei\Desktop")
-    Label(root,text="Downloaded Successfully").pack()
+        for widget in root.pack_slaves():
+            if isinstance(widget, Label) and widget.cget("text") == error_message:
+                return  # Exit to avoid dup
+        Label(root, text=error_message).pack()
 
+ 
 
 
 root = Tk()
 root.title("Fatih's Video Downloader")
-root.geometry("450x350")
+root.geometry("700x270")
 
 t = Text(root, height=1,width=45)
 
@@ -39,6 +50,7 @@ res1 = IntVar()
 res2 = IntVar()
 res3 = IntVar()
 text = StringVar(root, value="YouTube URL")
+exit = Button(root, text="Exit", command= root.destroy)
 
 
 
@@ -52,6 +64,7 @@ Checkbutton(text="360p",onvalue=18, offvalue=0, variable=res1).pack()
 Checkbutton(text="720p",onvalue=22, offvalue=0, variable=res2).pack()
 Checkbutton(text="1080p",onvalue=37, offvalue=0, variable=res3).pack()
 download.pack()
+exit.pack()
 
 
 root.mainloop()
